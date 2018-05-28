5db9ad67a62fad2a839b617c2ae6e9c4f4de0aed
xxyangyoulin <xxyangyoulin@gmail.com>
2018-05-28 09:09:58 +0800

    ☺️

---
+++ app/src/main/java/cn/xxyangyoulin/android_test_constraint_layout/MainActivity.java
@@ -0,0 +1,28 @@
+package cn.xxyangyoulin.android_test_constraint_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prop_layout);
+
+        /**
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         * 学习地址：
+         * https://blog.csdn.net/guolin_blog/article/details/53122387
+         *
+         * https://www.jianshu.com/p/a74557359882
+         */
+    }
+}
